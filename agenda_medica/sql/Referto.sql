a46380882246c9e357706e1bb95d2ce3fb94573d
Mittsommerregen <giulia.amato94@live.it>
2016-06-01 18:05:15 +0200

    Update and rename Referti.sql to Referto.sql

---
+++ agenda_medica/sql/Referto.sql
@@ -0,0 +1,6 @@
+INSERT INTO Referto(CodiceReferto, Testo) VALUES
+(1, "Ciclista investito da auto. Caduta a terra con trauma cranico, iniziale amnesia, attualmente ricorda l'accaduto:
+riferisce di essere stato stretto in una strettoia e di essere quindi stato toccato e caduto di conseguenza. Dolorabilità in 
+sede cervicale. Diagnosi: Frattura scomposta della clavicola sinistra, escoriazioni multiple. Ambulatorio: Pronto soccorso"),
+(2,"Esame eseguito con sequenze DP, T1W, T2W e con soppressione del grasso secondo piani multipli. Non sono apprezzabili alterazioni
+a livello delle strutture ossee dell'articolazione scapolo omerale. Si prescrive Artbxa28 20mg al giorno dopoi pasti.");
