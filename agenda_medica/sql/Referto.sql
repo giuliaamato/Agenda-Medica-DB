2b1a0893e305e0fd902ede209a61108b5e57e8d6
Mittsommerregen <giulia.amato94@live.it>
2016-06-02 12:47:17 +0200

    Update Referto.sql

--- agenda_medica/sql/Referto.sql
+++ agenda_medica/sql/Referto.sql
@@ -1,6 +1,4 @@
 INSERT INTO Referto(CodiceReferto, Testo) VALUES
-(1, "Ciclista investito da auto. Caduta a terra con trauma cranico, iniziale amnesia, attualmente ricorda l'accaduto:
-riferisce di essere stato stretto in una strettoia e di essere quindi stato toccato e caduto di conseguenza. Dolorabilità in 
-sede cervicale. Diagnosi: Frattura scomposta della clavicola sinistra, escoriazioni multiple. Ambulatorio: Pronto soccorso"),
-(2,"Esame eseguito con sequenze DP, T1W, T2W e con soppressione del grasso secondo piani multipli. Non sono apprezzabili alterazioni
-a livello delle strutture ossee dell'articolazione scapolo omerale. Si prescrive Artbxa28 20mg al giorno dopoi pasti.");
+(1, "Ciclista investito da auto. Caduta a terra con trauma cranico, iniziale amnesia, attualmente ricorda l'accaduto: riferisce di essere stato stretto in una strettoia e di essere quindi stato toccato e caduto di conseguenza. Dolorabilità in sede cervicale. Diagnosi: Frattura scomposta della clavicola sinistra, escoriazioni multiple. Ambulatorio: Pronto soccorso"),
+(2,"Esame eseguito con sequenze DP, T1W, T2W e con soppressione del grasso secondo piani multipli. Non sono apprezzabili alterazionia livello delle strutture ossee dell'articolazione scapolo omerale. Si prescrive Artbxa28 20mg al giorno dopo i pasti.");
+(3, "Esami ematochimici nella norma, non sono state riscontrate alterazioni. Prescrivo isotretinoina 10mg per un mese. Al termine del mese il paziente tornerà per un controllo. Ambulatorio: Dermatologia"
