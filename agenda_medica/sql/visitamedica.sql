91b2510a489eb40588190891ffc8f495c39420da
Mittsommerregen <giulia.amato94@live.it>
2016-06-02 13:25:22 +0200

    Update visitamedica.sql

--- agenda_medica/sql/visitamedica.sql
+++ agenda_medica/sql/visitamedica.sql
@@ -2,4 +2,4 @@ INSERT INTO VisitaMedica(CodiceVisita, Data, Ambulatorio, TipoVisita, TipoPrenot
 (1, '2016-03-10', 'Pronto Soccorso', 'Visita', 'Effettuata','M','MTRMTT63D19D118Q', 'NVNGLI87R41C383T', 'NGRALB80D24B578M', 1),
 (2, '2016-03-11', 'Ortopedia', 'Visita', 'Effettuata', 'L', 'OTTGSP69E21H220D', 'MTRQNT77H05B154Y', 'DMCRGN75H70L026P', 2),
 (3, '2016-3-12', 'Dermatologia', 'Visita', 'Effettuata', 'L', 'ANGORZ44D30D107M', 'ALLELI67E64D740J', 'ORLGVN83E03D506A', 2);
-(4, '2016-6-10', 'Dermatologia', 'Controllo', 'Prenotata', 'L', 'ANGORZ44D30D107M', NULL, 'ORLGVN83E03D506A', 2);
+(4, '2016-6-10', 'Dermatologia', 'Controllo', 'Prenotata', 'L', 'ANGORZ44D30D107M', NULL, 'ORLGVN83E03D506A', NULL);
