f7ae423e28e354139cc587b1b28730b506d1fe13
Paolo Broglio <pbroglio@gmail.com>
2016-06-02 18:05:55 +0200

    Corretti file dump

--- agenda_medica/sql/Referto.sql
+++ agenda_medica/sql/Referto.sql
@@ -1,4 +1,7 @@
-INSERT INTO Referto(CodiceReferto, Testo) VALUES
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE Referto;
+INSERT INTO Referto(Codice, Contenuto) VALUES
 (1, "Ciclista investito da auto. Caduta a terra con trauma cranico, iniziale amnesia, attualmente ricorda l'accaduto: riferisce di essere stato stretto in una strettoia e di essere quindi stato toccato e caduto di conseguenza. Dolorabilità in sede cervicale. Diagnosi: Frattura scomposta della clavicola sinistra, escoriazioni multiple."),
-(2,"Esame eseguito con sequenze DP, T1W, T2W e con soppressione del grasso secondo piani multipli. Non sono apprezzabili alterazionia livello delle strutture ossee dell'articolazione scapolo omerale. Si prescrive Artbxa28 20mg al giorno dopo i pasti.");
-(3, "Esami ematochimici nella norma, non sono state riscontrate alterazioni. Prescrivo isotretinoina 10mg per un mese. Al termine del mese il paziente tornerà per un controllo."
+(2,"Esame eseguito con sequenze DP, T1W, T2W e con soppressione del grasso secondo piani multipli. Non sono apprezzabili alterazionia livello delle strutture ossee dell'articolazione scapolo omerale. Si prescrive Artbxa28 20mg al giorno dopo i pasti."),
+(3, "Esami ematochimici nella norma, non sono state riscontrate alterazioni. Prescrivo isotretinoina 10mg per un mese. Al termine del mese il paziente tornerà per un controllo.");
+SET FOREIGN_KEY_CHECKS = 1;
--- agenda_medica/sql/dump_cup.sql
+++ agenda_medica/sql/dump_cup.sql
@@ -3,13 +3,13 @@ TRUNCATE CUP;
 
 INSERT INTO CUP (Codice,Password,CodiceASL) VALUES
 
-	("CVICENZA00","cupvicenza",0),
-	("CPADOVA01","cuppadova",1),
-	("CVENEZIA02","cupvenezia",2),
-	("CVERONA03","cupverona",3),
-	("CBELLUNO04","cupbelluno",4),
-	("CTREVISO05","cuptreviso",5),
-	("CROVIGO06","cuprovigo",6),
-	("CCHIOGGIA07","cupchioggia",7);
+	("CVICENZA00","cupvicenza",1),
+	("CPADOVA01","cuppadova",2),
+	("CVENEZIA02","cupvenezia",3),
+	("CVERONA03","cupverona",4),
+	("CBELLUNO04","cupbelluno",5),
+	("CTREVISO05","cuptreviso",6),
+	("CROVIGO06","cuprovigo",7),
+	("CCHIOGGIA07","cupchioggia",8);
 
 SET FOREIGN_KEY_CHECKS=1;
